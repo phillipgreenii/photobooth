a0a9941c298e8150ff6d1e48464e62f0adb9fab0
Phillip Green II <deathcheese@yahoo.com>
2011-05-13 02:45:56 -0400

    generated pdf has images (#4)

--- photo_printer.py
+++ photo_printer.py
@@ -9,7 +9,7 @@ class PhotoPrinter:
     def __init__(self):
         self.logger = logging.getLogger('photo.printer')
         self._printer = ''
-        self._pagesize = self._create_pagesize(2.2, 8.5)
+        self._pagesize = self._create_pagesize(2.2, 8.5) #TODO don't hard code page size
 
     def _create_pagesize(self,width,height):
         import reportlab.lib.pagesizes as ps
@@ -23,17 +23,26 @@ class PhotoPrinter:
         event_callback({'type':'PRINTED'})
 
     def _generatePdf(self,session):
-        file_name =  os.path.join(session.get_storage_directory(), session.get_name() + '.pdf') #TODO should create path or let session handle it?
+        file_name =  os.path.join(session.get_storage_directory(), session.get_name() + '.pdf') #TODO create local image and let sessin move it
         pdf = Canvas(file_name,pagesize=self._pagesize)
+        # add metadata
         pdf.setAuthor('photobooth') #TODO add app version to author
         pdf.setSubject('wedding photos')
         pdf.setTitle('pictures for session %s' % session.get_name())
         pdf.setKeywords(('wedding', 'pictures','photobooth'))
+
+        # add pictures
+        #TODO add padding
+        (total_width, total_height) = self._pagesize
+        (image_width, image_height) = (total_width, total_height / len(session.get_photos()))        
+        for (i,photo) in enumerate(session.get_photos()):
+            pdf.drawInlineImage(photo,0,i * image_height, image_width, image_height, preserveAspectRatio=True, anchor='n')
         
         pdf.showPage()
         pdf.save()
 
     def _printPdf(self,pdfLocation):
+        #TODO implement _printPdf
         pass
 
     def __repr__(self):
