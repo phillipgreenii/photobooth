b5f2d82b664b32da24add8c4e97e81055054fde0
Phillip Green II <deathcheese@yahoo.com>
2011-05-21 10:30:30 -0400

    refactored photo_taker to be more stateless (don't need to create a new one each time)

--- photo_taker.py
+++ photo_taker.py
@@ -3,66 +3,58 @@ import time
 import logging
 
 class PhotoTaker:
-    def __init__(self,camera, session, delayBetweenPhotos,  event_callback=lambda e : None):
+    def __init__(self,camera):
         self.logger = logging.getLogger('photo.taker')
         self.camera = camera
-        self.delay_between_photos = delayBetweenPhotos
-        self.session = session
-        self.event_callback = event_callback
         self.started = False
-        if self.logger.isEnabledFor(logging.DEBUG):
-            self.logger.debug(repr(self))
 
-    def start(self):
+    def takePictures(self, delay_between_photos, session, event_callback=lambda e : None):
         if self.started:
             raise exception('already started!')
         self.started = True
-        numberOfPictures = self.session.total_photos_to_take()
+        numberOfPictures = session.total_photos_to_take()
+
+        def delayForNextPicture(seconds):
+            self.logger.debug('delay before next picture')
+            for timeLeft in xrange(seconds,0,-1): 
+                event_callback({'type':'COUNT_DOWN_UPDATE', 'time_until_picture': timeLeft})
+                time.sleep(1) 
+            event_callback({'type':'COUNT_DOWN_UPDATE', 'time_until_picture': 0})
+
+        def takeNextPicture(counter):
+            self.logger.info('taking picture: %02d' % counter)        
+            picture_filename = '%012d' % math.floor(time.time()) + ".jpg"
+            self.camera.set_property("filename", picture_filename)
+            event_callback({'type':'TAKE_PICTURE', 'current_picture':counter, 'total_pictures':numberOfPictures})
+            self.camera.set_property('block-after-capture', True)
+            self.camera.emit("capture-start")
+            self.logger.debug('pausing')
+            time.sleep(1)
+            self.camera.set_property('block-after-capture', False)
+
+        def handlePicture(c,filename):
+                self.logger.debug('handling picture: %s' % filename)
+                session.addPhoto(filename)
+                if session.is_complete():
+                        self._cleanup()
                 
         self.logger.debug('starting to take pictures')
-        self.event_callback({'type':'START'})
-        self.handler_id = self.camera.connect("image-done",self._handlePicture)
+        event_callback({'type':'START'})
+        self.handler_id = self.camera.connect("image-done",handlePicture)
         try:
             for i in range(numberOfPictures): 
-                self._delayForNextPicture(self.delay_between_photos)
-                self._takeNextPicture(i,numberOfPictures)
-            self.event_callback({'type':'DONE'})
+                delayForNextPicture(delay_between_photos)
+                takeNextPicture(i+1)
+            event_callback({'type':'DONE'})
         except:
             #TODO log error
-            self.event_callback({'type':'ERROR'})
+            event_callback({'type':'ERROR'})
             self._cleanup()
             raise
-            
-    def _delayForNextPicture(self, seconds):
-        self.logger.debug('delay before next picture')
-        for timeLeft in xrange(seconds,0,-1): 
-            self.event_callback({'type':'COUNT_DOWN_UPDATE', 'time_until_picture': timeLeft})
-            time.sleep(1) 
-        self.event_callback({'type':'COUNT_DOWN_UPDATE', 'time_until_picture': 0})
-
-    def _takeNextPicture(self, counter, numberOfPictures):
-        self.logger.info('taking picture: %02d' % counter)        
-        picture_filename = '%012d' % math.floor(time.time()) + ".jpg"
-        self.camera.set_property("filename", picture_filename)
-        self.event_callback({'type':'TAKE_PICTURE', 'current_picture':counter+1, 'total_pictures':numberOfPictures})
-        self.camera.set_property('block-after-capture', True)
-        self.camera.emit("capture-start")
-        self.logger.debug('pausing')
-        time.sleep(1)
-        self.camera.set_property('block-after-capture', False)
-
-    def _handlePicture(self,c,filename):
-        self.logger.debug('handling picture: %s' % filename)
-        self.session.addPhoto(filename)
-        if self.session.is_complete():
-            self._cleanup()
-
+        finally:
+            self.started = False
 
     def _cleanup(self):
         self.logger.debug('cleaning up (%s)' % str(self.handler_id))
         if self.handler_id is not None:
             self.camera.disconnect(self.handler_id)
-
-    def __repr__(self):
-        return "%s(camera=%s,event_callback=%s,delay_between_photos=%d)" % \
-               (self.__class__,self.camera, self.event_callback, self.delay_between_photos)
--- photobooth_controller.py
+++ photobooth_controller.py
@@ -5,47 +5,46 @@ import logging
 
 class PhotoboothController:
 
-	def __init__(self, configuration, printer_manager, camera_manager):
-		self.logger = logging.getLogger('photobooth.controller')
+    def __init__(self, configuration, printer_manager, camera_manager):
+        self.logger = logging.getLogger('photobooth.controller')
             
-		self._apply_configuration(configuration)
-		self.printerManager = printer_manager
-                self.cameraManager = camera_manager
-		self.photoPrinter = photo_printer.PhotoPrinter(self.printerManager, self._printer_name)
+        self._apply_configuration(configuration)
+        self.printerManager = printer_manager
+        self.cameraManager = camera_manager
+        self.photoPrinter = photo_printer.PhotoPrinter(self.printerManager, self._printer_name)
+        self.photoTaker = photo_taker.PhotoTaker(self.cameraManager.camerabin)
 
-	def _apply_configuration(self,configuration):
-		def check_for_parameter(param):
-			if not param in configuration:
-				raise exception('required configuration paramater (%s) not found' % param)
-			value = configuration[param]
-			self.logger.debug('configuration: %s=%s' % (param, str(value)))
-			return value
-			
-		self._session_root_path = check_for_parameter('output-directory')
-		self._number_of_photos = check_for_parameter('number-of-photos')
-		self._time_delay = check_for_parameter('time-delay')
-		self._printer_name = check_for_parameter('printer')
+    def _apply_configuration(self,configuration):
+        def check_for_parameter(param):
+            if not param in configuration:
+                raise exception('required configuration paramater (%s) not found' % param)
+            value = configuration[param]
+            self.logger.debug('configuration: %s=%s' % (param, str(value)))
+            return value
+            
+        self._session_root_path = check_for_parameter('output-directory')
+        self._number_of_photos = check_for_parameter('number-of-photos')
+        self._time_delay = check_for_parameter('time-delay')
+        self._printer_name = check_for_parameter('printer')
 
+    def setViewFinder(self,viewFinder):
+        self.cameraManager.setViewFinder(viewFinder)
 
-	def setViewFinder(self,viewFinder):
-                self.cameraManager.setViewFinder(viewFinder)
+    def isCameraEnabled(self):
+        return self.cameraManager.isCameraEnabled()
 
-	def isCameraEnabled(self):
-		return self.cameraManager.isCameraEnabled()
+    def enableCamera(self):
+        self.cameraManager.enableCamera()
 
-	def enableCamera(self):
-                self.cameraManager.enableCamera()
+    def disableCamera(self):
+        self.cameraManager.disableCamera()
 
-	def disableCamera(self):
-		self.cameraManager.disableCamera()
+    def takePictures(self,event_callback = lambda e : None ):
+        self.logger.debug('creating session')
+        session = photo_session.PhotoSession(self._session_root_path, self._number_of_photos)
+        self.logger.info('Taking pictures for %s' % session)        
+        self.photoTaker.takePictures(self._time_delay, session, event_callback)
+        self.logger.info('Printing pictures')        
+        self.photoPrinter.printSession(session,event_callback)
 
-	def takePictures(self,event_callback = lambda e : None ):
-		self.logger.debug('creating session')
-		session = photo_session.PhotoSession(self._session_root_path, self._number_of_photos)
-		self.logger.info('Taking pictures for %s' % session)
-		photoTaker = photo_taker.PhotoTaker(self.cameraManager.camerabin, session, self._time_delay, event_callback)#TODO do I need a new photoTake each time?
-		photoTaker.start()
-		self.logger.info('Printing pictures')		
-		self.photoPrinter.printSession(session,event_callback)
-
-	
+    
